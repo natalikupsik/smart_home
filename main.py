d3e97459465bdbe0088a840e0376bf466b6b3e48
natalikupsik <nat9837@yandex.ru>
2016-11-20 23:46:05 +0300

    Удалены комментарии

--- main.py
+++ main.py
@@ -10,76 +10,7 @@ from TYPES import *
 from device import *
 
 #данный модуль запускает все процессы и сопоставляет очереди
-'''def work(QueueIn,QueueOut):
-    a = 0
-    while True:
-        if QueueIn.empty() == False:
-           cmd = CommandIO()
-           cmd = QueueIn.get()
-           cmd.PrintInfo()
 
-        if (a==0):
-            time.sleep(15)
-            a = 1
-            c = CommandIO()
-            c.cmd = "request"
-            c.type = "temp"
-            c.number = 0
-            c.data = "NULL"
-            c.PrintInfo()
-            QueueOut.put(c)
-
-'''
-
-'''
-rf = threading.Thread(target=work, args=(QueueIn,QueueOut))
-rf.start()
-StartGUI(QueueIn,QueueOut)
-'''
-
-'''
-QueueIn = Queue.Queue()
-QueueOut = Queue.Queue()
-
-StartModem(QueueIn,QueueOut,'/dev/cu.HUAWEIMobile-Pcui','/dev/cu.HUAWEIMobile-Modem')
-
-'''
-
-
-'''
-QueueInMainDeviceFromProtocol = Queue.Queue()
-QueueOutMainDeviceToProtocol = Queue.Queue()
-
-QueueInProtocolFromGSM = Queue.Queue()
-QueueOutProtocolToGSM = Queue.Queue()
-
-
-threadProtocol = threading.Thread(target=StartProtol, args=(QueueInProtocolFromGSM, QueueInMainDeviceFromProtocol,QueueOutMainDeviceToProtocol, QueueOutProtocolToGSM,))
-threadProtocol.start()
-
-threadGSM= threading.Thread(target=StartModem, args=(QueueOutProtocolToGSM,QueueInProtocolFromGSM,))
-threadGSM.start()
-
-test = threading.Thread(target=work, args=(QueueInMainDeviceFromProtocol,))
-test.start()
-
-'''
-
-'''
-QueueInFromGUI = Queue.Queue()
-QueueInFromMainDevice = Queue.Queue()
-QueueOutToGUI = Queue.Queue()
-QueueOutToMainDevice = Queue.Queue()
-
-threadIO = threading.Thread(target=StartIOmanager, args=(QueueInFromGUI, QueueInFromMainDevice, QueueOutToGUI, QueueOutToMainDevice,))
-threadIO.start()
-
-test = threading.Thread(target=work, args=(QueueOutToMainDevice,QueueInFromMainDevice))
-test.start()
-
-StartGUI(QueueOutToGUI,QueueInFromGUI)
-
-'''
 
 QueueFromGUIToIO = Queue.Queue()
 QueueFromIOToGUI = Queue.Queue()
